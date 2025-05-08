32d38f1a709c71adab3d00840de8c279f5e33f94
Jonathan Colombo <jonathan.colombo@covisian.com>
2025-05-08 11:44:43 +0200

    added files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+/**
+ * The entrypoint for the action. This file simply imports and runs the action's
+ * main logic.
+ */
+import { run } from './main.js'
+
+/* istanbul ignore next */
+run()
